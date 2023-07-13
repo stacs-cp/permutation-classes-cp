fa5c9578ea483fd5953ec65daf8c9b5234503259
Ruth Hoffmann <rh347@icloud.com>
2023-07-13 09:15:42 +0100

    changed input order for parallel

--- PP23/av1324inversioncount/params/run.py
+++ PP23/av1324inversioncount/params/run.py
@@ -21,7 +21,7 @@ def test(testMinionSeq,testMinionPar,testNBCSAT):
 inargs = sys.argv[1:]
 if len(inargs) == 0:
     print('Arguments need to be given')
-    print('<Maximal permutations length> <solver> <solver options, optional>')
+    print('<solver> <solver options, optional> <Maximal permutations length>')
     print('Solvers are minionseq , minionpar (which needs the number of processes as well), nbcsat')
     print('If no solver is given minionseq is standard')
 
@@ -31,9 +31,9 @@ elif inargs[0] == 'testmp':
     run([0,5],5,[True,2],False)
 elif inargs[0] == 'testnsat':
     run([0,5],5,[False],True)
-elif inargs[1] == 'minionseq':
-    run([0,comb(int(inargs[0]),2)],int(inargs[0]),[False],False)
-elif inargs[1] == 'minionpar':
-    run([0,comb(int(inargs[0]),2)],int(inargs[0]),[True,int(inargs[2])],False)
-elif inargs[1] == 'nbcsat':
-    run([0,comb(int(inargs[0]),2)],int(inargs[0]),[False],True)
+elif inargs[0] == 'minionseq':
+    run([0,comb(int(inargs[2]),2)],int(inargs[2]),[False],False)
+elif inargs[0] == 'minionpar':
+    run([0,comb(int(inargs[2]),2)],int(inargs[2]),[True,int(inargs[1])],False)
+elif inargs[0] == 'nbcsat':
+    run([0,comb(int(inargs[2]),2)],int(inargs[2]),[False],True)

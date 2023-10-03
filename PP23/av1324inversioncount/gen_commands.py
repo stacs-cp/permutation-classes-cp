64c375ec1f20da1170d2c4b4413a6ee64143d919
Özgür Akgün <ozgurakgun@gmail.com>
2023-10-03 13:34:47 +0100

    a bti more configurable: from_ and upto

--- PP23/av1324inversioncount/gen_commands.py
+++ PP23/av1324inversioncount/gen_commands.py
@@ -8,6 +8,7 @@ import os
 
 # Length:
 # - 1 to upto
+from_ = 1
 upto = 15
 
 # Number of inversions:
@@ -16,7 +17,7 @@ upto = 15
 
 with open("commands-seq.txt", "w") as out:
     for solver in ["minionseq", "nbcsat"]:
-        for length in range(1, upto+1):
+        for length in range(from_, upto+1):
             lengthPadded = str(length).zfill(2)
             for nbInv in range(0, math.comb(length, 2) + 1):
                 nbInvPadded = str(nbInv).zfill(3)
@@ -27,7 +28,7 @@ with open("commands-seq.txt", "w") as out:
 
 with open("commands-par.txt", "w") as out:
     for solver in ["minionpar"]:
-        for length in range(1, upto+1):
+        for length in range(from_, upto+1):
             lengthPadded = str(length).zfill(2)
             for nbInv in range(0, math.comb(length, 2) + 1):
                 nbInvPadded = str(nbInv).zfill(3)

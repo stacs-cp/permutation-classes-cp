771ba5625d633e800056f1377782f4be1f3591f2
Özgür Akgün <ozgurakgun@gmail.com>
2023-11-05 15:01:17 +0000

    nbInv=20 is done

--- PP23/av1324inversioncount/gen_commands.py
+++ PP23/av1324inversioncount/gen_commands.py
@@ -7,32 +7,43 @@ import os
 # - nbcsat
 
 # Length:
-# - 1 to upto
-from_ = 1
-upto = 16
+# - 1 to n
 
 # Number of inversions:
 # - 0 .. comb(length,2)
 
-
-# with open("commands-seq.txt", "w") as out:
-#     for solver in ["minionseq", "nbcsat"]:
-#         for length in range(from_, upto+1):
-#             lengthPadded = str(length).zfill(2)
-#             for nbInv in range(0, math.comb(length, 2) + 1):
-#                 nbInvPadded = str(nbInv).zfill(3)
-#                 infoFile = f'conjure-output/model000001-{solver}-{lengthPadded}-{nbInvPadded}.eprime-info'
-#                 if not os.path.exists(infoFile):
-#                     print(
-#                         f'python3 run.py {solver} {lengthPadded} {nbInvPadded}', file=out)
-
 with open("commands-par.txt", "w") as out:
-    for solver in ["minionpar"]:
-        for length in range(from_, upto + 1):
-            lengthPadded = str(length).zfill(2)
+    for solver in ["minionpar"]:                                    # other solvers: ["minionseq", "nbcsat"]:
+
+        # all nbInvs for lengths 1..16
+        for length in range(1, 16+1):
             for nbInv in range(0, math.comb(length, 2) + 1):
-                nbInvPadded = str(nbInv).zfill(3)
+                i = nbInv
+                lengthPadded = str(length).zfill(2)
+                nbInvPadded = str(i).zfill(3)
                 infoFile = f'conjure-output/model000001-{solver}-{lengthPadded}-{nbInvPadded}.eprime-info'
                 if not os.path.exists(infoFile):
-                    print(
-                        f'python3 run.py {solver} {lengthPadded} {nbInvPadded}', file=out)
+                    print(f'python3 run.py {solver} {lengthPadded} {nbInvPadded}', file=out)
+                    print(f'python3 run.py {solver} {lengthPadded} {nbInvPadded}')
+
+        # also: 25x10
+        for length in range(1, 25+1):
+            for nbInv in range(0, min(10, math.comb(length, 2)) + 1):
+                i = nbInv
+                lengthPadded = str(length).zfill(2)
+                nbInvPadded = str(i).zfill(3)
+                infoFile = f'conjure-output/model000001-{solver}-{lengthPadded}-{nbInvPadded}.eprime-info'
+                if not os.path.exists(infoFile):
+                    print(f'python3 run.py {solver} {lengthPadded} {nbInvPadded}', file=out)
+                    print(f'python3 run.py {solver} {lengthPadded} {nbInvPadded}')
+
+        # also: 20x20
+        for length in range(1, 20):
+            for nbInv in range(0, min(20, math.comb(length, 2)) + 1):
+                i = nbInv
+                lengthPadded = str(length).zfill(2)
+                nbInvPadded = str(i).zfill(3)
+                infoFile = f'conjure-output/model000001-{solver}-{lengthPadded}-{nbInvPadded}.eprime-info'
+                if not os.path.exists(infoFile):
+                    print(f'python3 run.py {solver} {lengthPadded} {nbInvPadded}', file=out)
+                    print(f'python3 run.py {solver} {lengthPadded} {nbInvPadded}')

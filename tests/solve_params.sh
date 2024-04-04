f57a6b32f2c5170278cb28e31c6c6c81024bbac2
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2024-04-04 19:39:53 +0100

    Fixing matrix

--- tests/solve_params.sh
+++ tests/solve_params.sh
@@ -2,7 +2,7 @@
 
 if [ "$1" = "containment" ]
   then
-    ./solve_classical_containment_params.sh
+    ./tests/solve_classical_containment_params.sh
   else
-    ./solve_classical_avoidance_params.sh
+    ./tests/solve_classical_avoidance_params.sh
 fi
